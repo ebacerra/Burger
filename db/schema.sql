599974272994ab88a7a848eb1101006e9411570a
Ellen <ellen.bacerra@gmail.com>
2018-09-13 23:05:14 -0500

    update

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE burgers_db;
-USE burgers_db;;
+USE burgers_db;
 
 -- Create the table plans.
 CREATE TABLE burgers
 (
-id INT NOT NULL AUTO_INCREMENT,
+id INT(10) NOT NULL AUTO_INCREMENT,
 burger_name VARCHAR(255) NOT NULL,
 devoured BOOLEAN NOT NULL,
 PRIMARY KEY (id)
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Double Cheeseburger", 0),("Portebello Mushroomburger", 0),("Veggieburger", 0);
