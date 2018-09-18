6624011c602c984fc5833b57998eda1406c2310f
Ellen <ellen.bacerra@gmail.com>
2018-09-17 22:38:45 -0500

    update

---
+++ READme.md
@@ -0,0 +1,24 @@
+# Burger
+---------------------------------
+>  Created a fullstack app "Devour The Burger" aka "The Flip-Shack". Utilizing Get/Post/Put in a MVC design pattern
+
+---------------------------------------------
+
+![image](/Burger.png)
+
+
+---------------------------------------------
+> * With Table creation
+> * Id
+> * Burger_Name
+> * Devoured
+
+
+---------------------------------------------
+Tech used:
+---------- 
+* Nodes.js
+* MySQL
+* Express
+* Handlebars
+* ORM
--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "root",
+        port: 8889,
+        database: "burgers_db"
 
+    });
+};
+
+// Make connection.
 connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
@@ -17,4 +23,7 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId);
 });
 
+// Export connection for our ORM to use.
 module.exports = connection;
+
+
